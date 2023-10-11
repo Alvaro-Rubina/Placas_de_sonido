b4d219ab92cce1da9b7429b366d7c68b7ac768d2
diegoCardenas03 <109765105+diegoCardenas03@users.noreply.github.com>
2023-10-10 21:10:30 -0300

    seguir cambiando el footer

--- Pagina/js/app.js
+++ Pagina/js/app.js
@@ -5,8 +5,33 @@ var typed = new Typed(".logo", {
     backDelay: 1000,
     loop: true
 });
-function mostrarIcono(elemento){
-    elemento.style.padding = '1vh'
-    let espacioAgregar = document.getElementById('iconos');
-    espacioAgregar.innerHTML = "<i class='bi bi-instagram' id='instagram-icon'></i> <i class='bi bi-github id' id='github-icon'></i>";
+function mostrarIconos(elemento) {
+    const iconos = elemento.querySelector('.iconos-ig-gh');
+    const nombre = elemento.querySelector('.nombre');
+
+    // Mostrar los iconos con animación
+    iconos.style.opacity = 1;
+    iconos.style.transform = 'translateY(0)';
+    iconos.style.transition = 'opacity 0.3s, transform 0.3s';
+
+    // Guardar la posición original del nombre
+    nombre.dataset.originalTranslateY = window.getComputedStyle(nombre).transform;
+    
+    // Aplicar una transformación para mantener el nombre en su lugar
+    nombre.style.transform = 'translateY(' + nombre.offsetTop + 'px)';
+    nombre.style.transition = 'transform 0.3s';
+}
+
+function ocultarIconos(elemento) {
+    const iconos = elemento.querySelector('.iconos-ig-gh');
+    const nombre = elemento.querySelector('.nombre');
+
+    // Ocultar los iconos con animación
+    iconos.style.opacity = 0;
+    iconos.style.transform = 'translateY(20px)';
+    iconos.style.transition = 'opacity 0.3s, transform 0.3s';
+
+    // Restaurar la posición original del nombre
+    nombre.style.transform = nombre.dataset.originalTranslateY;
+    nombre.style.transition = 'transform 0.3s';
 }
